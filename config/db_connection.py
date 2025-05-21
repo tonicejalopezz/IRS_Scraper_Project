a867c79d80cc7921a91a9f161684b792fbceae4c
tonicejalopezz <jcejalop@uoregon.edu>
2025-05-21 16:40:33 -0700

    Updated May 21

--- config/db_connection.py
+++ config/db_connection.py
@@ -3,39 +3,34 @@ from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 import pandas as pd
 
-def fetch_engine(database_name):
-    """
-    Datbase connnection in PostgreSQL using SQLAlchemy.
-    """
-    load_dotenv()
+class DatabaseConnect:
+
+    def __init__(self, target_db):
+        self.target_db = target_db
+
+        load_dotenv()
+        self.db_user = os.getenv("DB_USER")
+        self.db_password = os.getenv("DB_PASSWORD")
+        self.db_host = os.getenv("DB_HOST")
+        self.db_port = os.getenv("DB_PORT")
+        self.db_name = os.getenv(target_db)
     
-    db_user = os.getenv("DB_USER")
-    db_password = os.getenv("DB_PASSWORD")
-    db_host = os.getenv("DB_HOST")
-    db_port = os.getenv("DB_PORT")
-    db_name = os.getenv(database_name)
+    def connect(self):
+        """
+        Connecting to PostgreSQL databse.
+        """
+        connection = f"postgresql+psycopg2://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+        try:
+            self.engine = create_engine(connection)
+            with self.engine.connect() as con:
+                con.execute(text("SELECT 1"))
+            print("Database Successfully Connected.")
+            return self.engine
+        except Exception as e:
+            print(f"Database Connection Failed: {e}")
 
-    connection = f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
-
-    try:
-    
-        engine = create_engine(connection)
-        with engine.connect() as con:
-
-            con.execute(text("SELECT 1"))
-        print("Database Successfully Connected.")
-        return engine
-
-    except Exception as e:
-
-        print("Database Connection Failed.")
-        print(f"Error; {e}")
-        return None
-
-def main():
-    
-    db_name = input("Enter the database name: ")
-    engine = fetch_engine(db_name)
-
-if __name__ == "__main__":
-    main()
+    def get_engine(self):
+        """
+        Function to consider if an engine is already connected.
+        """
+        self.engine
